cb2d5d22dde7edb942c3554cb26f3893ec419fe6
eswarreddykarri999-afk <eswarreddykarri999@gmail.com>
2025-10-22 19:23:37 +0530

    show sql & refactor

--- project1-hibernate-basics/src/main/java/org/example/Main.java
+++ project1-hibernate-basics/src/main/java/org/example/Main.java
@@ -10,20 +10,27 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
         Student s1 = new Student();
-        s1.setRollNo(101);
-        s1.setsName("Harsh");
-        s1.setsAge(21);
+        s1.setRollNo(103);
+        s1.setsName("Rani");
+        s1.setsAge(25);
 
-        Configuration cfg = new Configuration();
-        cfg.addAnnotatedClass(org.example.Student.class);
-        cfg.configure();
+//        Configuration cfg = new Configuration();
+//        cfg.addAnnotatedClass(org.example.Student.class);
+//        cfg.configure();
 
-        SessionFactory sf = cfg.buildSessionFactory();
+        SessionFactory sf = new Configuration()
+                .addAnnotatedClass(org.example.Student.class)
+                .configure()
+                .buildSessionFactory();
+                //cfg.buildSessionFactory();
         Session session = sf.openSession();
 
         Transaction transaction = session.beginTransaction();
         session.persist(s1);
         transaction.commit();
+
+        session.close();
+        sf.close();
 
         System.out.println(s1);
 
